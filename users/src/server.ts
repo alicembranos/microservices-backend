c005b1724154dce3ae1a973a8bd0562dc1af022f
RogerOliveDelgado <rogerolivedelgado@gmail.com>
2022-10-30 17:00:29 +0100

    feat: :sparkles: Cors Policy
    Apply CORS on Nginx Configuration and delete it from Server Definition

--- users/src/server.ts
+++ users/src/server.ts
@@ -12,7 +12,7 @@ dotenv.config();
 export default async (app) => {
 	app.use(morgan("dev"));
 	app.use(helmet());
-	app.use(cors());
+	// app.use(cors());
 	app.use(express.json({ limit: "50mb" }));
 	app.use(express.urlencoded({ extended: true }));
 	app.use(swaggerUi.serve);
