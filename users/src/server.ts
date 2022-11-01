95cd7dba3e1c5fe6807a2942210d43df044bcb27
RogerOliveDelgado <rogerolivedelgado@gmail.com>
2022-11-01 11:59:40 +0100

    Apply cors on server

--- users/src/server.ts
+++ users/src/server.ts
@@ -11,7 +11,7 @@ dotenv.config();
 export default async (app) => {
 	app.use(morgan("dev"));
 	app.use(helmet());
-	// app.use(cors());
+	app.use(cors());
 	app.use(express.json({ limit: "50mb" }));
 	app.use(express.urlencoded({ extended: true }));
 
